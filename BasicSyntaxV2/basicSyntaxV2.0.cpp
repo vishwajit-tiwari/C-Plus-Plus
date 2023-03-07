312aad6c2d8e6767c09afc5aab492f4d59282bd2
vishwajit-tiwari <vishwajitkumar37@gmail.com>
2023-03-07 15:06:23 +0530

    Basic syntax v2 of C++

---
+++ BasicSyntaxV2/basicSyntaxV2.0.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+    cout<<"Hello world using name space std\n";
+    cout<<"@Author: Vishwajit Tiwari\n";
+    
+    return 0;
+}
