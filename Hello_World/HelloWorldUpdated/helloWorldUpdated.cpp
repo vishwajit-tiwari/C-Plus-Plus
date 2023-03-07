d5d7f83e7d6af1f9ecb0fb94b487ea3fcaff0e39
vishwajit-tiwari <vishwajitkumar37@gmail.com>
2023-03-07 15:08:08 +0530

    Hello World Program

---
+++ Hello_World/HelloWorldUpdated/helloWorldUpdated.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file helloWorldUpdated.cpp
+ * @author your name (you@domain.com)
+ * @brief Another way to write Hello World!
+ * @version 0.1
+ * @date 2022-09-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Hello World!\n";
+    cout<<"Hello World-2!\n";
+    return 0;
+}
---
+++ Hello_World/helloWorld.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file helloWorld.cpp
+ * @author your name (you@domain.com)
+ * @brief My First Program in C++ 
+ * @version 0.1
+ * @date 2022-09-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include<iostream>
+
+int main()
+{
+    std :: cout<<"Hello World!";
+
+    return 0;
+}
